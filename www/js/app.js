6d65b1226f7be34e72e4fd0fe7d80ac6f498d2b1
Ed Solis <edsfocci@gmail.com>
2016-01-23 18:48:41 -0600

    Not enough pylons

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,9 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// angular.module is a global place for creating, registering and retrieving
+// Angular modules
+// 'starter' is the name of this angular module example (also set in a <body>
+// attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
@@ -9,10 +11,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    // Hide the accessory bar by default (remove this to show the accessory bar
+    // above the keyboard for form inputs)
+    if(window.cordova && window.cordova.plugins &&
+      window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -38,47 +42,107 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
     // Each tab has its own nav history stack:
 
-    .state('tab.dash', {
-      url: '/dash',
+    .state('tab.menu', {
+      url: '/menu',
       views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+        'tab-menu': {
+          templateUrl: 'templates/tab-menu.html',
+          controller: 'MenuCtrl'
         }
       }
     })
 
-    .state('tab.friends', {
-      url: '/friends',
+    .state('tab.shop', {
+      url: '/shop',
       views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
+        'tab-shop': {
+          templateUrl: 'templates/tab-shop.html',
+          controller: 'ShopCtrl'
         }
       }
     })
 
-    .state('tab.account', {
-      url: '/account',
+    .state('tab.cart', {
+      url: '/cart',
       views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
+        'tab-cart': {
+          templateUrl: 'templates/tab-cart.html',
+          controller: 'CartCtrl'
+        }
+      }
+    })
+
+    .state('tab.tools', {
+      url: '/tools',
+      views: {
+        'tab-tools': {
+          templateUrl: 'templates/tab-tools.html',
+          controller: 'ToolsCtrl'
+        }
+      }
+    })
+
+    .state('tab.mylowes-signin', {
+      url: '/mylowes-signin',
+      views: {
+        'mylowes': {
+          templateUrl: 'templates/mylowes-signin.html',
+          controller: 'MyLowesSignInCtrl'
+        }
+      }
+    })
+
+    .state('tab.mylowes-signup', {
+      url: '/mylowes-signup',
+      views: {
+        'mylowes': {
+          templateUrl: 'templates/mylowes-signup.html',
+          controller: 'MyLowesSignUpCtrl'
         }
       }
     });
 
+    // .state('tab.dash', {
+    //   url: '/dash',
+    //   views: {
+    //     'tab-dash': {
+    //       templateUrl: 'templates/tab-dash.html',
+    //       controller: 'DashCtrl'
+    //     }
+    //   }
+    // })
+
+    // .state('tab.friends', {
+    //   url: '/friends',
+    //   views: {
+    //     'tab-friends': {
+    //       templateUrl: 'templates/tab-friends.html',
+    //       controller: 'FriendsCtrl'
+    //     }
+    //   }
+    // })
+    // .state('tab.friend-detail', {
+    //   url: '/friend/:friendId',
+    //   views: {
+    //     'tab-friends': {
+    //       templateUrl: 'templates/friend-detail.html',
+    //       controller: 'FriendDetailCtrl'
+    //     }
+    //   }
+    // })
+
+    // .state('tab.account', {
+    //   url: '/account',
+    //   views: {
+    //     'tab-account': {
+    //       templateUrl: 'templates/tab-account.html',
+    //       controller: 'AccountCtrl'
+    //     }
+    //   }
+    // });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/mylowes-signin');
 
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,27 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
+.controller('MenuCtrl', function($scope) {})
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+.controller('ShopCtrl', function($scope) {})
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+.controller('CartCtrl', function($scope) {})
 
-.controller('AccountCtrl', function($scope) {
-});
+.controller('ToolsCtrl', function($scope) {})
+
+.controller('MyLowesSignInCtrl', function($scope) {})
+
+.controller('MyLowesSignUpCtrl', function($scope) {});
+
+// .controller('DashCtrl', function($scope) {
+// })
+
+// .controller('FriendsCtrl', function($scope, Friends) {
+//   $scope.friends = Friends.all();
+// })
+
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
+
+// .controller('AccountCtrl', function($scope) {
+// });
